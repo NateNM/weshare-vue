01feecd89047bd0a496e5c38aaf40f64ffc70efa
Nathaniel Nate Mguli <111265578+NateNM@users.noreply.github.com>
2024-09-18 12:31:27 +0200

    Add Vite configuration file and dependencies

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # weshare-vue
+
+# The expense tracker will allow users to:
+- Track their expenses
+- Borrow from others and lend to others
+- Link expenses to specific members
+
+# Tech Stack
+- Frontend: Vue.js, Vue Router, Vuex (for state management), Vite
+- Backend: Node.js
+- Database: MongoDB
+- Authentication: JWT (JSON Web Tokens) for user authentication
+- Styling: Tailwind CSS
---
+++ src/router/index.ts
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+// import Home from '../views/Home.vue';
+// import About from '../views/About.vue';
+// import Login from '../views/Login.vue';
+
+const routes: Array<RouteRecordRaw> = [  
+  {
+    path: '/',
+    name: 'Home',
+    component: Login
+  },
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+});
+
+export default router;
